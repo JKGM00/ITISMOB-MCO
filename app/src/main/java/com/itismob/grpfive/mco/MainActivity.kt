5eb05a2207b016ec15b0ff741a1f9d0027389095
JKGM00 <150331813+JKGM00@users.noreply.github.com>
2025-10-13 22:13:07 +0800

    Add main/register layouts and update dependencies
    Introduced ConstraintLayout-based activity_main.xml and activity_register.xml layouts, replacing the previous LinearLayout. Added color, string, and theme resources for UI consistency. Updated MainActivity to use the new main layout, renamed logo image, removed unused POS layout, and added androidx.constraintlayout dependency.

--- app/src/main/java/com/itismob/grpfive/mco/MainActivity.kt
+++ app/src/main/java/com/itismob/grpfive/mco/MainActivity.kt
@@ -3,12 +3,14 @@ package com.itismob.grpfive.mco
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import com.itismob.grpfive.mco.R
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
+        setContentView(R.layout.activity_main)
 
 
 
